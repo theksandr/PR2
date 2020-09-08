cf5c93bbe182b0b4bee3eb52a75cd7240558bf03
Саша Панов <alexpanov2001@gmail.com>
2020-09-08 10:29:20 +0300

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,10 @@
+public class Book
+{
+    String title="TheBook", author="TheAuthor";
+    int year = 2020, pages = 100;
+
+    public void main(String[] args)
+    {
+        System.out.println(title + "/n" + author + "/n" + year + "/n" + pages);
+    }
+}
